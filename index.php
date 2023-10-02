c453ffb4e9922addf7057dd9f3984c6337da1092
giuseppevignanello <peppe.vignanello@gmail.com>
2023-10-02 13:07:08 +0200

    add login and register basic structure

--- index.php
+++ index.php
@@ -5,11 +5,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edusogno</title>
+    <link rel="stylesheet" href="./assets/styles/style.css">
+    <title>Edusogno - Login</title>
 </head>
 
 <body>
-    test
+    <header class="app_header bg_white d_flex align_items_center">
+        <div class="logo">
+            <img src="https://edusogno.com/logo-black.svg" alt="">
+        </div>
+    </header>
+
+    <main>
+        <div class="login_box">
+            <form action="" method="post"></form>
+            <input type="mail">
+            <input type="password">
+            <button type="submit">Submit</button>
+        </div>
+
+        <hr>
+        <a href="register.php">Register</a>
+    </main>
 </body>
 
 </html>
---
+++ register.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/styles/style.css">
+    <title>Edusogno - Register</title>
+</head>
+
+<body>
+    <header class="app_header bg_white d_flex align_items_center">
+        <div class="logo">
+            <img src="https://edusogno.com/logo-black.svg" alt="">
+        </div>
+    </header>
+    <main>
+        <div class="register_box">
+            <form action="" method="post"></form>
+            <label for="name_input_register">Inserisci il nome</label>
+            <input type="text" id="name_input_register">
+            <label for="surname_input_register">Inserisci il cognome</label>
+            <input type="text" id="surname_input_register">
+            <label for="mail_input_register">Inserisci la mail</label>
+            <input type="mail" id="mail_input_register">
+            <label for="password_input_register">Inserisci la password</label>
+            <input type="password" id="password_input_register">
+            <button type="submit">Submit</button>
+
+        </div>
+    </main>
+
+</body>
+
+</html>
